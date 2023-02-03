7613159df19e94f61f96cecbb20b28be263df9a5
crowdbotics-dev <engineering@crowdbotics.com>
2023-02-03 17:49:55 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+class Product(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=200,)
+    price = models.IntegerField()
+    description = models.TextField()
+    product_type = models.CharField(max_length=50,choices=None,)
